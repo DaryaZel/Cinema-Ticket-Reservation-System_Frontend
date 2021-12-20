08ed0a5debad3cf256f19a1e874b04497986955e
Darya <darya@MacBook-Pro-Aliaksei-2.local>
2021-12-20 13:43:04 +0300

    sign up modal window UI

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,12 @@
 import { LandingPage } from './pages/LandingPage/LandingPage';
 import './App.css';
+import { ModalWindow } from './pages/components/ModalWindow/ModalWindow';
 
 function App() {
   return (
     <div className="App">
       <LandingPage />
+      <ModalWindow/>
     </div>
   );
 }
---
+++ src/App/pages/components/ModalWindow/ModalWindow.js
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import './ModalWindow.css';
+import name from './images/user.png';
+import email from './images/arroba.png';
+import password from './images/lock.png';
+
+export function ModalWindow() {
+    const [inputTextName, setInputTextName] = useState('');
+    const [inputTextEmail, setInputTextEmail] = useState('');
+    const [inputTextPassword, setInputTextPassword] = useState('');
+    const signUpData = [
+        { title: 'Name', img: name, placeholder: 'type your username', inputText: inputTextName, setText: setInputTextName },
+        { title: 'Email', img: email, placeholder: 'type your email', inputText: inputTextEmail, setText: setInputTextEmail },
+        { title: 'Password', img: password, placeholder: 'type your password', inputText: inputTextPassword, setText: setInputTextPassword }
+    ];
+    const handleChange = (text, setInputText) => {
+        setInputText(text)
+    };
+
+    return (
+        <div className='modal'>
+            <div className='modal__content'>
+                <div className='modal__title'>
+                    <h2>Sign Up</h2>
+                </div>
+                <form>
+                    <div className='modal__container'>
+                        {
+                            signUpData.map((item) => (
+                                <div className='modal__row'>
+                                    <div className='modal__row-title'>
+                                        <span>{item.title}</span>
+                                    </div>
+                                    <div className='modal__input-container'>
+                                        <div className='modal__input-icon'>
+                                            <img src={item.img} />
+                                        </div>
+                                        <input type='text' value={item.inputText} placeholder={item.placeholder} className='modal__input' onChange={(event) => handleChange(event.target.value, item.setText)}></input>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </div>
+                    <div >
+                        <button className='modal__button'>
+                            <span>Sign Up</span>
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
