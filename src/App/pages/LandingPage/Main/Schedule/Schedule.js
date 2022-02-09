ca7a7003372ee8fc68d5cdc14ecc0fe95c895094
Darya <darya@MacBook-Pro-Aliaksei-2.local>
2022-02-09 22:31:32 +0300

    implement no sessions display

--- src/App/pages/LandingPage/Main/Schedule/Schedule.js
+++ src/App/pages/LandingPage/Main/Schedule/Schedule.js
@@ -43,7 +43,9 @@ export function Schedule({ city, cinema, day }) {
                         <div className='schedule__date-container'>
                             <h2>{new Date(elem.day).toLocaleDateString(locale, formattingOptions)}</h2>
                         </div>
-                        {
+
+                        {elem.schedules.length === 0 ?
+                            <h3 className='schedule__no-sessions'>No sessions found</h3> :
                             elem.schedules.map((cinema) => (
                                 <CinemaSchedule cinema={cinema} />
                             ))
